bc02cc8c3728e98bb6afa9d0cee926e5ab13c953
keeiisee <1769493@alu.murciaeduca.es>
2023-05-18 20:46:48 +0200

    guardar

--- fronted/src/containers/Perfil/ActualizarPerfil.jsx
+++ fronted/src/containers/Perfil/ActualizarPerfil.jsx
@@ -44,38 +44,45 @@ function ActualizarPerfil({ load_Idprofile, modificar_perfil, user }) {
         modificar_perfil(imagen, descripcion, logros, user, profile[0].id)
         navigate('/profile')
     };
-    
+    Navbarperfil
     return (
         <div>
             <Navbar></Navbar>
-            <div className="container py-5">
-                <h1 className="mb-4">Modificar Perfil</h1>
-                <form action="#" method="POST" encType="multipart/form-data">
-                    <div className="mb-3">
-                        <label htmlFor="imagen" className="form-label">Imagen:</label>
-                        <input type="file" className="form-control" onChange={handleImagenChange} id="imagen" name="imagen" placeholder='ejemplo uno' />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="descripcion" className="form-label">Descripción:</label>
-                        <textarea className="form-control" onChange={handleDescripcionChange} id="descripcion" name="descripcion" value={descripcion} rows="3"></textarea>
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Logros:</label>
-                        <select className="form-select" onChange={handleLogrosChange} name="logros" multiple>
-                            {opcionesLogros && opcionesLogros.map((logro, key) => {
-                                if (profile && profile[0].logros.includes(logro)){
-                                    return <option key={key} value={logro} selected>{logro}</option>
-                                } else {
-                                    return <option key={key} value={logro}>{logro}</option>
-                                }
-                               
-                            })}
-                        </select>
-                    </div>
+            <div className="container-fluid vh-100 h-md-50">
+                <div className="row h-100">
+                    <Navbarperfil></Navbarperfil>
+                    
+                    <div className="col-md-8">
+                        <br />
+                    <h1 className="mb-4">Modificar Perfil</h1>
+                    <form action="#" method="POST" encType="multipart/form-data">
+                        <div className="mb-3">
+                            <label htmlFor="imagen" className="form-label">Imagen:</label>
+                            <input type="file" className="form-control" onChange={handleImagenChange} id="imagen" name="imagen" placeholder='ejemplo uno' />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="descripcion" className="form-label">Descripción:</label>
+                            <textarea className="form-control" onChange={handleDescripcionChange} id="descripcion" name="descripcion" value={descripcion} rows="3"></textarea>
+                        </div>
+                        <div className="mb-3">
+                            <label className="form-label">Logros:</label>
+                            <select className="form-select" onChange={handleLogrosChange} name="logros" multiple>
+                                {opcionesLogros && opcionesLogros.map((logro, key) => {
+                                    if (profile && profile[0].logros.includes(logro)) {
+                                        return <option key={key} value={logro} selected>{logro}</option>
+                                    } else {
+                                        return <option key={key} value={logro}>{logro}</option>
+                                    }
 
-                    <button type="submit" onClick={handleSubmit} className="btn btn-primary">Guardar</button>
-                </form>
+                                })}
+                            </select>
+                        </div>
+
+                        <button type="submit" onClick={handleSubmit} className="btn btn-primary">Guardar</button>
+                    </form>
+                </div>
             </div>
+        </div>
         </div>
     )
 }

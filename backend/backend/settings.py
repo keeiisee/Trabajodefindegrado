eb5b8c8c8af438e98a855ba1ebb083eb08ca21aa
keeiisee <1769493@alu.murciaeduca.es>
2023-06-03 20:03:09 +0200

    prueba antes del desastre1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -169,7 +169,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
 STATIC_URL = '/static/'
 CORS_ORIGIN_ALLOW_ALL = True
 MEDIA_URL = 'media/'
